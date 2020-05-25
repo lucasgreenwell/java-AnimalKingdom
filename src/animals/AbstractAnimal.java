7efae31c6cddb1a424a77a70730f9ff00d3f1ae2
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-05-25 01:09:35 -0600

    set up abstract class for overarching animal class

--- src/animals/AbstractAnimal.java
+++ src/animals/AbstractAnimal.java
@@ -1,2 +1,41 @@
 package animals;
 
+public abstract class AbstractAnimal {
+    public static int maxId = 0;
+    int id;
+    String name;
+    int yearDiscovered;
+
+    public AbstractAnimal(String name, int yearDiscovered){
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public abstract String move();
+    public abstract String breathe();
+    public abstract String reproduce();
+
+    public String eat(){
+        return "Crunch";
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getYearDiscovered(){
+        return this.yearDiscovered;
+    }
+
+    public void setYearDiscovered(int yearDiscovered){
+        this.yearDiscovered = yearDiscovered;
+    }
+}
